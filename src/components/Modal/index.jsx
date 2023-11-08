7bfc575684d3d0d50ee986153934599be6ac0bd7
paulacynthia <pcynthia013@gmail.com>
2023-11-08 02:00:51 -0300

    :bug: correcting modal behavior

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,47 +1,26 @@
 import { Poppins_500Medium, useFonts } from "@expo-google-fonts/poppins";
+import { Feather } from "@expo/vector-icons";
 
 import React, { useState } from "react";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 
-import { Feather } from "@expo/vector-icons";
-import { Modal, StyleSheet, Text, View } from "react-native";
+import Modal from "react-native-modal";
+
 import { Colors } from "../../utilities/Colors";
 import { Button } from "../Button";
 
+const windowHeight = Dimensions.get("window").height;
+
 export const ModalCustom = ({ title, children }) => {
   const [fontsLoaded, fontError] = useFonts({ Poppins_500Medium });
-  const [modalVisible, setModalVisible] = useState(false);
+  const [isModalVisible, setModalVisible] = useState(false);
 
   if (!fontsLoaded && !fontError) {
     return null;
   }
 
   return (
-    <View style={[styles.main, modalVisible && styles.bgLowOpacity]}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>{title}</Text>
-            <View style={styles.inputs}>{children}</View>
-            <Button
-              title="Salvar"
-              type="secondary"
-              onPress={() => setModalVisible(!modalVisible)}
-            />
-            <Button
-              title="Cancelar"
-              type="cancel"
-              onPress={() => setModalVisible(!modalVisible)}
-            />
-          </View>
-        </View>
-      </Modal>
+    <View style={styles.main}>
       <Button
         title={title}
         type="secondary"
@@ -49,6 +28,33 @@ export const ModalCustom = ({ title, children }) => {
       >
         <Feather name="plus" size={20} color={Colors.BLUE} />
       </Button>
+
+      <Modal
+        isVisible={isModalVisible}
+        style={styles.modalCustom}
+        deviceWidth="100%"
+      >
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>{title}</Text>
+
+          <ScrollView>
+            <View style={styles.inputs}>{children}</View>
+          </ScrollView>
+
+          <View style={styles.buttons}>
+            <Button
+              title="Salvar"
+              type="secondary"
+              onPress={() => setModalVisible(!isModalVisible)}
+            />
+            <Button
+              title="Cancelar"
+              type="cancel"
+              onPress={() => setModalVisible(!isModalVisible)}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -58,42 +64,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "end",
     alignItems: "center",
-    position: 'absolute',
-    bottom: 0,
+    paddingVertical: 10,
   },
-  bgLowOpacity: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'rgba(0, 0, 0 / 0.5)',
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "end",
-    alignItems: "center",
+  modalCustom: {
+    width: "full",
+    margin: 0,
+    justifyContent: "flex-end",
   },
   modalView: {
-    width: '100%',
+    width: "100%",
     gap: 16,
     borderTopLeftRadius: 34,
     borderTopRightRadius: 34,
-    padding: 50,
+    padding: 30,
+    bottom: 0,
     backgroundColor: Colors.WHITE_300,
-  },
-  button: {
-    alignSelf: "center",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 6,
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-    flex: 1,
-    width: 350,
-  },
-  textStyle: {
-    color: Colors.BLUE,
-    fontWeight: "bold",
-    textAlign: "center",
   },
   modalText: {
     fontFamily: "Poppins_500Medium",
@@ -102,6 +87,10 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   inputs: {
+    maxHeight: windowHeight / 2,
     gap: 20,
+  },
+  buttons: {
+    marginTop: 20,
   },
 });
